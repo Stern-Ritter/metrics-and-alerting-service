ce3d02311fb2ffc1b99a9e9c4e8793282da8ed40
Stern_Ritter <fadeef.k@yandex.ru>
2024-03-21 14:39:00 +0300

    feat: add requests logging to the server
    Added requests logging for all current endpoints
    The request logging information contains: URI, request method,
    time to complete request, response status code, size of response
    content
    Logging implemented through middleware.

--- internal/app/server/app.go
+++ internal/app/server/app.go
@@ -4,21 +4,34 @@ import (
 	"net/http"
 
 	config "github.com/Stern-Ritter/metrics-and-alerting-service/internal/config/server"
+	logger "github.com/Stern-Ritter/metrics-and-alerting-service/internal/logger/server"
 	service "github.com/Stern-Ritter/metrics-and-alerting-service/internal/service/server"
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 func Run(s *service.Server) error {
-	config, err := getConfig(config.ServerConfig{})
+	config, err := getConfig(config.ServerConfig{
+		LoggerLvl: "info",
+	})
+	if err != nil {
+		return err
+	}
+
+	err = logger.Initialize(config.LoggerLvl)
 	if err != nil {
 		return err
 	}
 
 	router := chi.NewRouter()
+	router.Use(logger.RequestLogger)
 	router.Get("/", s.GetMetricsHandler)
 	router.Post("/update/{type}/{name}/{value}", s.UpdateMetricHandler)
 	router.Get("/value/{type}/{name}", s.GetMetricHandler)
 
 	err = http.ListenAndServe(config.URL, router)
+	if err != nil {
+		logger.Log.Fatal(err.Error(), zap.String("event", "start server"))
+	}
 	return err
 }
