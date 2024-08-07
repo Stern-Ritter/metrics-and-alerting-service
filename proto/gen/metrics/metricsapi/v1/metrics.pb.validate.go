8b9474f4af4b9a100483c44e2c1af45a345839fe
Stern_Ritter <fadeef.k@yandex.ru>
2024-08-08 00:22:16 +0300

    fix: fix go vet

--- proto/gen/metrics/metricsapi/v1/metrics.pb.validate.go
+++ proto/gen/metrics/metricsapi/v1/metrics.pb.validate.go
@@ -4,35 +4,9 @@
 package v1
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"net"
-	"net/mail"
-	"net/url"
-	"regexp"
-	"sort"
 	"strings"
-	"time"
 	"unicode/utf8"
-
-	"google.golang.org/protobuf/types/known/anypb"
-)
-
-// ensure the imports are used
-var (
-	_ = bytes.MinRead
-	_ = errors.New("")
-	_ = fmt.Print
-	_ = utf8.UTFMax
-	_ = (*regexp.Regexp)(nil)
-	_ = (*strings.Reader)(nil)
-	_ = net.IPv4len
-	_ = time.Duration(0)
-	_ = (*url.URL)(nil)
-	_ = (*mail.Address)(nil)
-	_ = anypb.Any{}
-	_ = sort.Sort
 )
 
 // Validate checks the field values on MetricData with the rules defined in the
